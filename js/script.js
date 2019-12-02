5c68ab7bf57f686dc7120e782135f06f0b480fdb
Lynor Wilson <36920398+Lynsanity83@users.noreply.github.com>
2019-12-02 02:16:42 -0500

    edited style of skills section.

--- js/script.js
+++ js/script.js
@@ -14,38 +14,12 @@
         showCursor: false,
     });
 
-    var owl = $('.owl-carousel');
-        owl.owlCarousel({
-            items:4,
-            loop:true,
-            margin:10,
-            autoplay:{
-                0:{
-                    items:1
-                },
-                480:{
-                    items:2
-                },
-                768:{
-                    items:3
-                },
-                938:{
-                    items:4
-                }
-            },
-            autoplayTimeout:3000,
-            autoplayHoverPause:true
-        });
-        $('.play').on('click',function(){
-            owl.trigger('play.owl.autoplay',[1000])
-        })
-        $('.stop').on('click',function(){
-            owl.trigger('stop.owl.autoplay')
-        })
-
-   /*$('.owl-carousel').owlCarousel({
+   $('.owl-carousel').owlCarousel({
+        autoplay:true,
+        autplayTimeout:1000,
+        autoplayHoverPause: true,
         loop:true,
-        items: 4,
+        items:4,
         responsive:{
             0:{
                 items:1
@@ -60,20 +34,24 @@
                 items:4
             }
         }
-    })*/
+    })
 
-    $(function() {
-        $('.chart').easyPieChart({
-            easing: 'easeInOut',
-            barColor: '#fff',
-            trackColor: false,
-            scaleColor: false,
-            lineWidth: 4,
-            size: 152,
-            onStep: function(from, to, percent){
-                $(this.el).find('.percent').text(Math.round(percent));
-            }
-        });
+    var skillsTopOffset = $(".skillsSection").offset().top;
+    $(window).scroll(function() {
+        if(window.pageYOffset > skillsTopOffset - $(window).height() + 200){
+
+            $('.chart').easyPieChart({
+                easing: 'easeInOut',
+                barColor: '#fff',
+                trackColor: false,
+                scaleColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function(from, to, percent){
+                    $(this.el).find('.percent').text(Math.round(percent));
+                }
+            });
+        }
     });
 
 });
